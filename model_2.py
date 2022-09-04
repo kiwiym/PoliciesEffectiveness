93575fb6e14744e31dd32817cf607263d94f5fae
kiwiym <112823157+kiwiym@users.noreply.github.com>
2022-09-04 19:09:23 +0100

    Add files via upload

---
+++ model_2.py
@@ -0,0 +1,72 @@
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+from load_data import *
+
+print("-"*5 + "13th Five-Year Plan" + "-"*5)
+
+# Policy Value
+v_16 = {}
+for i, prov in enumerate(provs):
+    s = np.zeros((5, 2))
+    c = np.zeros((5, 2))
+    for p, m, p_score in zip(p_prov_16[i], m_prov_16[i], s_prov_16[i]):
+        if m > 2:
+            continue
+        s[p-1][m-1] += p_score
+        c[p-1][m-1] += 1
+    c[c == 0] = 1
+    s[:, 0] = s[:, 0] * betas_16[i, 0]
+    s[:, 1] = s[:, 1] * betas_16[i, 1]
+    v_16[prov] = (s / c).flatten() * intensity_16[i]
+
+# Prepare x for Linear Regression
+x = np.array([v_16[p] for p in prov_19])
+y = np.array(int_dec_rate)
+
+# Fit Model
+model = LinearRegression(fit_intercept=True) # With Constant
+model.fit(x, y)
+print("Model:")
+print(model.coef_, model.intercept_)
+result_135 = model.predict(x)
+
+print("Result:")
+print(result_135)
+
+# Use Model to Predict for 14^th Five Year Plan
+
+print("-"*5 + "14th Five-Year Plan" + "-"*5)
+
+# Policy Value
+v_21 = {}
+for i, prov in enumerate(provs):
+    s = np.zeros((5, 2))
+    c = np.zeros((5, 2))
+    for p, m, p_score in zip(p_prov_21[i], m_prov_21[i], s_prov_21[i]):
+        if m > 2:
+            continue
+        s[p-1][m-1] += p_score
+        c[p-1][m-1] += 1
+    c[c == 0] = 1
+    s[:, 0] = s[:, 0] * betas_21[i, 0]
+    s[:, 1] = s[:, 1] * betas_21[i, 1]
+    v_21[prov] = (s / c).flatten() * intensity_19[i]
+
+# Prepare x for test
+x_21 = np.array([v_21[p] for p in prov_19])
+
+result_145 = model.predict(x_21)
+
+print("Result:")
+print(result_145)
+
+# Output Result into File
+df = pd.DataFrame(data={
+    "Province": prov_19_eng,
+    "13th": result_135,
+    "14th": result_145
+}).set_index("Province")
+df.to_csv("result_model_2.csv")
+print("\nResult dumped into file `result_model_2.csv`")
